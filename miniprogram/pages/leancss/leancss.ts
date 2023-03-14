4a9d9fc48215d9d905f278d9c4294b6156dc7e50
mark.yan <m15901368529@gmail.com>
2023-03-14 13:48:01 +0800

    css学习

---
+++ miniprogram/pages/leancss/leancss.ts
@@ -0,0 +1,30 @@
+// pages/leancss/leancss.ts
+Page({
+
+ data: {
+   showPath: true,
+   values: [
+{
+  name: 'jhon',
+  id: 1
+},
+{
+  name: 'mary',
+  id: 2
+},
+{
+  name: 'Mark',
+  id: 3
+},
+{
+  name: 'Teresa',
+  id: 5
+},
+{
+  name: 'jerry',
+  id: 4
+}
+   ],
+ }
+  
+})
